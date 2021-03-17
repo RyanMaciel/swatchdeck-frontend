8011545657fca6b32ff952e28267b60e47499a5d
Ryan Maciel <rspams1234@gmail.com>
2021-03-17 11:02:26 -0400

    Refactored out post logic to a hook, planning to abstract some of the issues with firebase into more hooks.

---
+++ src/hooks/useSubmitPost.ts
@@ -0,0 +1,39 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useStorage, useUser, useFirestore } from 'reactfire';
+import 'firebase/firestore';
+
+export function useSubmitPost(){
+  // Create a root reference
+  var storageRef = useStorage().ref();
+
+  const imageIdentifier = uuidv4();
+  const postIdentifier = uuidv4();
+
+  const { data: user } = useUser();
+  const firestore = useFirestore();
+
+  
+  const submitPost = (files:FileList)=>{
+    const timeStamp =  new Date().toUTCString();
+    firestore.collection('Posts').doc(postIdentifier).set({
+      title: 'This is a post',
+      imageIdentifier: imageIdentifier,
+      timeStamp: timeStamp,
+      userId: user.uid,
+    }).then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+        console.error("Error writing document: ", error);
+    });
+
+    if(files){
+      const imageRef = storageRef.child(imageIdentifier + ".jpg");
+      const file = files[0]
+      imageRef.put(file).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+      });
+    }
+  }
+  return [submitPost];
+}
--- src/pages/Post/Post.tsx
+++ src/pages/Post/Post.tsx
@@ -1,53 +1,38 @@
 import React from 'react';
 import 'firebase/storage';
 import HeaderToolbar from '../../common/HeaderToolbar';
+import {useSubmitPost} from '../../hooks/useSubmitPost';
 import { useStorage, useUser, useFirestore } from 'reactfire';
 import 'firebase/firestore';
 import { v4 as uuidv4 } from 'uuid';
 
 function Post(){
-
-  // Create a root reference
-  var storageRef = useStorage().ref();
-
-  // Create a reference to 'mountains.jpg'
-  const imageIdentifier = uuidv4();
-  const postIdentifier = uuidv4();
-
-  const { data: user } = useUser();
-  const firestore = useFirestore();
-  console.log(user);
-  const inputChange = (event:React.ChangeEvent<HTMLInputElement>)=>{
-
-
-    const timeStamp =  new Date().toUTCString();
-    firestore.collection('Posts').doc(postIdentifier).set({
-      title: 'This is a post',
-      imageIdentifier: imageIdentifier,
-      timeStamp: timeStamp,
-      userId: user.uid,
-    }).then(() => {
-      console.log("Document successfully written!");
-    })
-    .catch((error) => {
-        console.error("Error writing document: ", error);
-    });
-
-
+  const [submitPost] = useSubmitPost();
+  const onFileUpload = (event: React.ChangeEvent<HTMLInputElement>)=>{
     const files = event.target.files;
     if(files){
-      const imageRef = storageRef.child(imageIdentifier + ".jpg");
-      const file = files[0]
-      imageRef.put(file).then((snapshot) => {
-        console.log('Uploaded a blob or file!');
-      });
+      submitPost(files);
     }
-
   }
+  type InputDescriptor = {
+    label: string;
+    name: string;
+  }
+  let fields:Array<InputDescriptor> = [
+    {label: 'Title: ', name: 'title_input'},
+    {label: 'Description: ', name: 'description_input'}
+  ]
   return(
     <HeaderToolbar showPostButton={false}>
       <div>
-      <input type="file" onChange={inputChange} />
+        {fields.map((field:InputDescriptor)=>(
+          <div style={{display:'flex'}}>
+            <span>{field.label}</span>
+            <input type="text" id={field.name} name={field.name} required/>
+        </div>
+        ))}
+        
+        <input type="file" onChange={onFileUpload} />
       </div>
     </HeaderToolbar>
   )
